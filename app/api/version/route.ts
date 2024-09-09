7c6d448c6a8defaabe400eeb5974091329921904
tunafysh <hanansmani@gmail.com>
2024-09-09 12:52:22 +0200

    updated the versioning to semver and made it simpler

---
+++ app/api/version/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(req: NextRequest){
+    let latestversion: string= process.env.CATALYST_VERSION as string;
+    return NextResponse.json(latestversion)
+}
+
+export function POST(req: NextRequest) {
+    let key = process.env.CATALYST_KEY as string;
+    req.json().then((data) => {
+       if (data.key === key) {
+            process.env.CATALYST_VERSION = data.version
+        }
+    });
+    return NextResponse.json({status: "success"})
+}
--- middleware.ts
+++ middleware.ts
@@ -2,29 +2,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import { Catalyst } from './components/definitions/defs'
 
 export function middleware(req: NextRequest) {
-  let latestversion: string= '1.0.2';
+  let latestversion: string= process.env.CATALYST_VERSION as string;
   const url = req.nextUrl
   const agentpattern = /Catalyst\/(Windows|Unix)\/\d\.\d\.\d\/(check|update)/
   let useragent = req.headers.get("User-Agent")
 
-  if (useragent?.includes("Github")){
-
-  const webhookSecret = process.env.WEBHOOK_SECRET;
-  const githubEvent = req.headers.get('x-github-event');
-  const githubSignature = req.headers.get('x-hub-signature-256');
-  if (webhookSecret && githubEvent === 'release' && githubSignature) {
-    req.text().then((event: string) => {
-      if (event) {
-        const eventData = JSON.parse(event);
-        if (eventData.ref === 'refs/heads/main') {
-          if (eventData.repository.tags[0].name.match(/v\d.\d.\d/g))
-            latestversion = eventData.repository.tags[0].name.replace('v', '');
-        }
-    }});
-  }
-  else if (webhookSecret && githubEvent === "ping" && githubSignature) {
-    console.log("pong")
-  }
   if (useragent?.includes("PowerShell")){
     return NextResponse.rewrite(url.origin+"/scripts/install.ps1")
   }
@@ -54,5 +36,4 @@ export function middleware(req: NextRequest) {
       return cly.checkupd(latestversion)
     }
   }
-}
 }

6f239820d98757c4baa3fa64288af5d34b286927
tunafysh <hanansmani@gmail.com>
2024-09-10 14:22:40 +0200

    more fixes

--- middleware.ts
+++ middleware.ts
@@ -7,33 +7,35 @@ export function middleware(req: NextRequest) {
   const agentpattern = /Catalyst\/(Windows|Unix)\/\d\.\d\.\d\/(check|update)/
   let useragent = req.headers.get("User-Agent")
 
-  if (useragent?.includes("PowerShell")){
-    return NextResponse.rewrite(url.origin+"/scripts/install.ps1")
-  }
-  if (useragent?.includes("curl")){
-    return NextResponse.rewrite(url.origin+"/scripts/install")
-  }
-  if (useragent?.match(agentpattern)) {
-    const agent = useragent.match(agentpattern)![0]
-    const cly = new Catalyst()
-    cly.parse(agent)
-    if (cly.action == "update") {
-      const updateResponse = cly.update(latestversion, url);
-      if (updateResponse instanceof Response) {
-        return updateResponse;
-      } else {
-        switch (updateResponse) {
-          case "updatewin":
-            return NextResponse.rewrite(url.origin+"/scripts/update.ps1")
-          case "updateunix":
-            return NextResponse.rewrite(url.origin+"/scripts/update")
-          default:
-            return NextResponse.next();
+  if (req.method === "GET") {
+    if (useragent?.includes("PowerShell")){
+      return NextResponse.rewrite(url.origin+"/scripts/install.ps1")
+    }
+    if (useragent?.includes("curl")){
+      return NextResponse.rewrite(url.origin+"/scripts/install")
+    }
+    if (useragent?.match(agentpattern)) {
+      const agent = useragent.match(agentpattern)![0]
+      const cly = new Catalyst()
+      cly.parse(agent)
+      if (cly.action == "update") {
+        const updateResponse = cly.update(latestversion, url);
+        if (updateResponse instanceof Response) {
+          return updateResponse;
+        } else {
+          switch (updateResponse) {
+            case "updatewin":
+              return NextResponse.rewrite(url.origin+"/scripts/update.ps1")
+            case "updateunix":
+              return NextResponse.rewrite(url.origin+"/scripts/update")
+            default:
+              return NextResponse.next();
+          }
         }
       }
-    }
-    else {
-      return cly.checkupd(latestversion)
+      else {
+        return cly.checkupd(latestversion)
+      }
     }
   }
 }
